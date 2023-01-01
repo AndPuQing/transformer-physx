de1e2acad2ca4ff5b9cae392145a50ea9362dc6b
PuQing <me@puqing.work>
2023-01-01 12:17:54 +0800

    update

--- trphysx/utils/trainer.py
+++ trphysx/utils/trainer.py
@@ -295,7 +295,7 @@ class Trainer:
         model.train()
         for k, v in inputs.items():
             if isinstance(v, paddle.Tensor):
-                inputs[k] = v.to(self.args.src_device)
+                inputs[k] = v
 
         # Training head forward
         outputs = model(**inputs)
